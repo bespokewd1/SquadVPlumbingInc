5dfc6cb9dbc572fe150efc0b359e264bc43ad578
churchilldotexe <churchillexe@gmail.com>
2026-03-23 22:21:59 +0800

    chore: updated client domain

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,23 @@
 module.exports = {
-    name: "Squad V Plumbing Inc.",
-    email: "SQUADV726@gmail.com",
-    phoneForTel: "780-667-7169",
-    phoneFormatted: "(780)-667-7169",
-    address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
-    },
-    socials: {
-        facebook: "https://www.facebook.com/squadvplumbing/",
-        instagram: "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
-    // Passing the isProduction variable for use in HTML templates
-    isProduction: process.env.ELEVENTY_ENV === "PROD",
+  name: "Squad V Plumbing Inc.",
+  email: "SQUADV726@gmail.com",
+  phoneForTel: "780-667-7169",
+  phoneFormatted: "(780)-667-7169",
+  address: {
+    lineOne: "First Address Line",
+    lineTwo: "Second Address Line",
+    city: "Denver",
+    state: "CO",
+    zip: "80206",
+    country: "US",
+    mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/squadvplumbing/",
+    instagram: "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.squadvplumbing.ca",
+  // Passing the isProduction variable for use in HTML templates
+  isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
